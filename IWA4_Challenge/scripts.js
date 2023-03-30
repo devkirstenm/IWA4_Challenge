6cbcfd366c50c9ea05bfe911a19b475f59082ddd
kirstenm99 <125977448+kirstenm99@users.noreply.github.com>
2023-03-30 16:32:14 +0200

    Completed challenge

---
+++ IWA4_Challenge/scripts.js
@@ -0,0 +1,36 @@
+const date = 2050
+const status = 'student'
+const count = 0
+
+if (date = 2050) {
+	console.log("January", 'New Year`s Day')
+	console.log("March", 'Human Rights Day')
+	let date = 'April'
+	console.log(date, 'Family Day')
+	console.log(date, 'Freedom Day')
+	let count = count + 4
+
+	if (status = 'student') {
+	  console.log('June', 'Youth Day')
+		let count = count + 1
+  }
+
+	console.log('August', 'Womens Day')
+	console.log('September', 'Heritage Day')
+	let date = 'December'
+	console.log(date, 'Day of Reconciliation')
+	let count = count + 3
+
+	if (status = "parent") {
+	  console.log(date, 'Christmas Day')
+		let count = count + 1
+  }
+
+	console.log(date, 'Day of Goodwill')
+	let count = count + 1
+
+    console.log('Your status is:', status)
+    console.log('The year is:', date)
+    console.log('The total holidays is:', count)
+}
+
